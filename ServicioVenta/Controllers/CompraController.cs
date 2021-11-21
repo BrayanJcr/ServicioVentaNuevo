f2d6eb1bfc41a0e0e227f2d74ea0f600f8e82bcf
BrayanJcr <70727399+BrayanJcr@users.noreply.github.com>
2021-11-20 19:34:22 -0500

    Agregar archivos de proyecto.

---
+++ ServicioVenta/Controllers/CompraController.cs
@@ -0,0 +1,56 @@
+﻿using CapaDatos;
+using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicioVenta.Controllers
+{
+    public class CompraController : Controller
+    {
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        // GET: Compra
+        public ActionResult Consultar()
+        {
+            return View();
+        }
+
+        public ActionResult Documento(int idcompra = 0) {
+            
+            Compra oCompra = CD_Compra.Instancia.ObtenerDetalleCompra(idcompra);
+
+            if (oCompra == null) {
+                oCompra = new Compra();
+            }
+
+
+            return View(oCompra);
+        }
+
+
+        public JsonResult Obtener(string fechainicio, string fechafin, int idproveedor, int idtienda)
+        {
+            List<Compra> lista = CD_Compra.Instancia.ObtenerListaCompra(Convert.ToDateTime(fechainicio), Convert.ToDateTime(fechafin), idproveedor, idtienda);
+            return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult Guardar(string xml)
+        {
+            xml = xml.Replace("!idusuario¡", "1");
+
+            bool respuesta  = CD_Compra.Instancia.RegistrarCompra(xml);
+
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
---
+++ ServicioVenta/Helpers/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using CapaDatos;
+using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicioVenta.Helpers
+{
+    public static class Helpers
+    {
+       
+    }
+}
