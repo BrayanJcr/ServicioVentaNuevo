9a2289bfd936b476494b7521c873387cb8de0fa2
pc <75484126@continental.edu.pe>
2021-11-23 22:31:03 -0500

    log funcionando

--- ServicioVenta/App_Start/RouteConfig.cs
+++ ServicioVenta/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ServicioVenta
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Compra", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- ServicioVenta/Controllers/LoginController.cs
+++ ServicioVenta/Controllers/LoginController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using VentasWeb.Utilidades;
+using ServicioVenta.Utilidades;
 
 namespace ServicioVenta.Controllers
 {
